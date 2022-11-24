08a68f0fec0b11e3c77c760374a65ac1aff667d2
Valentin Charpentier <valentincharpentier@hotmail.com>
2022-11-24 10:11:50 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,38 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import Header from '../components/Header';
+
+// redux imports
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import bookmarks from '../reducers/bookmarks';
+
+// redux-persist imports
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
+const reducers = combineReducers({ bookmarks });
+const persistConfig = { key: 'hackatweet 12543', storage };
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});
+
+const persistor = persistStore(store);
 
 function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Next.js App</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Head>
+          <title>Hackatweet</title>
+        </Head>
+        <Header />
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
   );
 }
 
